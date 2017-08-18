0b293799950ff320cb80b05d855b1820de2acb4f
Emmanuel Tsouris <EmmanuelTsouris@users.noreply.github.com>
2017-08-18 16:42:11 -0700

    Create Install-Atom.ps1

---
+++ Install-Atom.ps1
@@ -0,0 +1,35 @@
+[CmdletBinding()]
+param(
+    [Parameter()]
+    [switch]$ShowUI
+)
+
+if ($IsLinux -or $IsOSX) { 
+    Write-Error "This script is currently supported only for Windows!"
+    return
+}
+
+[string]$URI = "https://atom.io/download/windows_x64"
+[string]$FileName = "AtomSetup-x64.exe"
+
+[string]$filePath = (Join-Path $env:TEMP $FileName)
+
+try {
+    $ProgressPreference = 'SilentlyContinue'
+
+    Write-Host "`nDownloading $FileName..."
+
+    Remove-Item -Force $filePath -ErrorAction SilentlyContinue
+    Invoke-Webrequest -Uri $downloadUrl -OutFile $filePath
+
+    Write-Host "`nInstalling Atom..."
+    if ($ShowUI) {
+        Start-Process $installerExePath -Wait
+    }
+    else {
+        Start-Process $installerExePath -ArgumentList "--silent" -Wait
+    }
+}
+finally {
+    $ProgressPreference = 'Continue'
+}
